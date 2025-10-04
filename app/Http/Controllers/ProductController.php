604fb69672304493fe3d111816235663ce7ff429
MyProject007 <myprojectaaa007@gmail.com>
2025-10-04 15:37:41 +0700

    BWA/Website-Apotik/WorkHarder:CodingStoreDataProduct

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('admin.products.index', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.products.create', [
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validasi data
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'about' => 'required|string',
+            'category_id' => 'required|integer',
+            'price' => 'required|integer',
+            'photo' => 'required|image|mimes:png,jpg,svg',
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            if ($request->hasFile('photo')) {
+                $photoPath = $request->file('photo')->store('product_photos', 'public');
+                $validated['photo'] = $photoPath;
+            }
+            $validated['slug'] = Str::slug($request->name);
+            $newProduct = Product::create($validated);
+
+            DB::commit();
+
+            return redirect()->route('admin.products.index');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $error = ValidationException::withMessages([
+                'system_error' => ['System error!' . $e->getMessage()],
+            ]);
+            throw $error;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
